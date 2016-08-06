5cea4bb5516ee0dc204cc371dbf196a31fc638cf
1ohm <deathroomba@gmail.com>
2016-08-06 17:20:15 +0900

    Added an answer of ID 3.

--- problem003.c
+++ problem003.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 #define NUM 600851475143
 
 void main (void){
-  int i,j;
-  int ans = 1;
-  int num = sqrt(NUM);
-  int sieve[num+1];
+  int i;
+  unsigned long long ans = NUM;
 
-  printf("%d\n",num);
-
-  for (i=2; i<num; i++) sieve[i]=1;
-  for (i=2; i<num; i++){
-    ans = i;
-    for(j=2; i*j<num; j++) sieve[i*j]=0;
+  for(i=2; i<=sqrt(NUM); i++){
+    while (ans%i == 0 && ans/i != 1) ans /= i;
   }
-  printf("%d\n", ans);
-  
-  /*
-  for(i=1;i<=num;i++){
-    if(num%i == 0) ans = i;
-    printf("%d %d %d\n", i, num%i, ans);
-    }*/
-
-  //  printf("%d", ans);
+  printf("%lld\n",ans);
 }
